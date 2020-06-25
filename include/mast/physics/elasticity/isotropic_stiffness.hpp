527ca6b4802cdaee4c7bad5920f15383d7f6b4e7
Manav Bhatia <bhatiamanav@gmail.com>
2020-06-25 12:59:29 -0500

    -- Added isotropic material -- Added linear strain energy

---
+++ include/mast/physics/elasticity/isotropic_stiffness.hpp
@@ -0,0 +1,151 @@
+
+#ifndef __mast_isotropic_material_stiffness_h__
+#define __mast_isotropic_material_stiffness_h__
+
+// MAST includes
+#include <mast/base/mast_data_types.h>
+
+
+namespace MAST {
+namespace Physics {
+namespace Elasticity {
+
+
+template <typename ScalarType, uint_t Dim, typename ModulusType, typename PoissonType, typename ContextType>
+class IsotropicMaterialStiffness;
+
+template <typename ScalarType, typename ModulusType, typename PoissonType, typename ContextType>
+class IsotropicMaterialStiffness<ScalarType, 2, ModulusType, PoissonType, ContextType> {
+public:
+    
+    using E_scalar_t  = typename ModulusType::scalar_t
+    using nu_scalar_t = typename PoissonsType::scalar_t
+    using scalar_t    = MAST::DeducedScalarType<MAST::DeducedScalarType<E_scalar_t, nu_scalar_t>::type, ScalarType>::type;
+    using value_t     = typename Eigen::Matrix<scalar_t, 3, 3>;
+    
+    IsotropicMaterialStiffness(const ModulusType& E,
+                               const PoissonType& nu):
+    _E     (E),
+    _nu    (nu) { }
+    
+    virtual ~IsotropicMaterialProperty(){}
+    
+    inline void value(const ContextType& c, value_t& m) const {
+                
+        const E_scalar_t
+        E  = _E.value(c);
+        const nu_scalar_t
+        nu = _nu.value(c);
+        
+        m.setZero();
+        
+        m(0, 0) = m(1, 1) = E/(1.-nu*nu);
+        m(0, 1) = m(1, 0) = E*nu/(1.-nu*nu);
+        m(2, 2) = E/2./(1.+nu);
+    }
+    
+    
+    template <typename ScalarFieldType>
+    inline void derivative(const ContextType&     c,
+                           const ScalarFieldType& f,
+                           value_t&               m) const {
+        
+        const E_scalar_t
+        E      = _E.value(c),
+        dEdp   = _E.derivative(c, f);
+
+        const nu_scalar_t
+        nu     = _nu.value(c),
+        dnudp  = _nu.derivative(c, f);
+
+        m.setZero();
+
+        m(0, 0) = m(1, 1) =
+        1./(1.-nu*nu) * dEdp + E/pow(1.-nu*nu,2) * nu * dnudp;
+        
+        m(0, 1) = m(1, 0) =
+        nu/(1.-nu*nu) * dEdp + (E/(1.-nu*nu) + E*nu/pow(1.-nu*nu,2)*nu) * dnudp;
+        
+        m(2, 2) =
+        1./2./(1.+nu)*dEdp - E/2./pow(1.+nu,2) * dnudp;
+    }
+
+protected:
+    
+    const ModulusType&  _E;
+    const PoissonType& _nu;
+};
+
+
+
+template <typename ScalarType, typename ModulusType, typename PoissonType, typename ContextType>
+class IsotropicMaterialStiffness<ScalarType, 3, ModulusType, PoissonType, ContextType> {
+public:
+    
+    using E_scalar_t  = typename ModulusType::scalar_t
+    using nu_scalar_t = typename PoissonsType::scalar_t
+    using scalar_t    = MAST::DeducedScalarType<MAST::DeducedScalarType<E_scalar_t, nu_scalar_t>::type, ScalarType>::type;
+    using value_t     = typename Eigen::Matrix<scalar_t, 6, 6>;
+    
+    IsotropicMaterialStiffness(const ModulusType& E,
+                               const PoissonType& nu):
+    _E     (E),
+    _nu    (nu) { }
+    
+    virtual ~IsotropicMaterialProperty(){}
+    
+    inline void value(const ContextType& c, value_t& m) const {
+                
+        const E_scalar_t
+        E  = _E.value(c);
+        const nu_scalar_t
+        nu = _nu.value(c);
+        
+        m.setZero();
+        
+        m(0, 0) = m(1, 1) = m(2, 2) = E*(1.-nu)/(1.-nu-2.*nu*nu);
+        m(0, 1) = m(0, 2) = m(1, 0) = m(1, 2) = m(2, 0) = m(2, 1) = E*nu/(1.-nu-2.*nu*nu);
+        m(3, 3) = m(4, 4) = m(5, 5) = E/2./(1.+nu);
+    }
+    
+    
+    template <typename ScalarFieldType>
+    inline void derivative(const ContextType&     c,
+                           const ScalarFieldType& f,
+                           value_t&               m) const {
+        
+        const E_scalar_t
+        E      = _E.value(c),
+        dEdp   = _E.derivative(c, f);
+
+        const nu_scalar_t
+        nu     = _nu.value(c),
+        dnudp  = _nu.derivative(c, f);
+
+        m.setZero();
+
+        m(0, 0) = m(1, 1) = m(2, 2) =
+        (1.-nu)/(1.-nu-2.*nu*nu) * dEdp +
+        (-E/(1.-nu-2.*nu*nu) + E*(1.-nu)/pow(1.-nu-2.*nu*nu,2)*(1.+4.*nu)) * dnudp ;
+        
+        m(0, 1) = m(0, 2) = m(1, 0) = m(1, 2) = m(2, 0) = m(2, 1) =
+        nu/(1.-nu-2.*nu*nu) * dEdp +
+        (E/(1.-nu-2.*nu*nu) + E*nu/pow(1.-nu-2.*nu*nu,2)*(1.+4.*nu)) * dnudp;
+        
+        m(3, 3) = m(4, 4) = m(5, 5) =
+        1./2./(1.+nu)*dEdp - E/2./pow(1.+nu,2) * dnudp;
+    }
+
+protected:
+    
+    const ModulusType&  _E;
+    const PoissonType& _nu;
+};
+
+
+} // namespace Elasticity
+} // namespace Physics
+} // namespace MAST
+
+
+#endif // __mast_isotropic_material_stiffness_h__
---
+++ include/mast/physics/elasticity/linear_strain_energy.hpp
@@ -0,0 +1,172 @@
+
+#ifndef __mast_linear_continuum_strain_energy_h__
+#define __mast_linear_continuum_strain_energy_h__
+
+// MAST includes
+#include <mast/physics/elasticity/linear_elastic_strain_operator.hpp>
+
+namespace MAST {
+namespace Physics {
+namespace Elasticity {
+
+
+
+template <typename FEVarType,
+          typename SectionPropertyType,
+          uint_t Dim,
+          typename ContextType>
+class LinearContinuumStrainEnergy {
+    
+public:
+    
+    template <uint_t D> struct NStrainComponents { };
+    template <> struct NStrainComponents<1> { static const uint_t value = 1; };
+    template <> struct NStrainComponents<2> { static const uint_t value = 3; };
+    template <> struct NStrainComponents<3> { static const uint_t value = 6; };
+
+    using scalar_t         = typename FEVarType::scalar_t;
+    using basis_scalar_t   = typename FEVarType::fe_basis_t::scalar_t;
+    using vector_t         = typename Eigen::Matrix<scalar_t, Eigen::Dynamic, Eigen::Dynamic>;
+    using matrix_t         = typename Eigen::Matrix<scalar_t, Eigen::Dynamic, Eigen::Dynamic>;
+    using section_scalar_t = typename SectionPropertyType::scalar_t;
+    using fe_basis_t       = typename FEVarType::fe_basis_t;
+    static const uint_t n_strain  = NStrainComponents<Dim>::value;
+    
+    LinearContinuumStrainEnergy():
+    _property    (nullptr),
+    _fe_var_data (nullptr)
+    { }
+    
+    virtual ~LinearContinuumStrainEnergy() { }
+
+    virtual inline void
+    set_section_property(const SectionPropertyType& p) {
+        
+        Assert0(!_property, "Property already initialized.");
+        
+        _property = &p;
+    }
+
+    virtual inline void set_fe_var_data(const FEVarType& fe_data)
+    {
+        Assert0(!_fe_var_data, "FE data already initialized.");
+        _fe_var_data = &fe_data;
+    }
+
+    virtual inline uint_t n_dofs() const {
+
+        Assert0(_fe_var_data, "FE data not initialized.");
+
+        return Dim*_fe_var_data->get_fe_shape_data().n_basis();
+    }
+    
+    inline void compute(ContextType& c,
+                        vector_t& res,
+                        matrix_t* jac = nullptr) const {
+        
+        Assert0(_fe_var_data, "FE data not initialized.");
+        Assert0(_property, "Section property not initialized");
+        
+        const typename FEVarType::fe_basis_t
+        &fe = _fe_var_data->get_fe_shape_data();
+        
+        typename Eigen::Matrix<scalar_t, n_strain, 1>
+        epsilon,
+        stress;
+        typename Eigen::Matrix<scalar_t, Eigen::Dynamic, 1>
+        vec     = Eigen::Matrix<scalar_t, Eigen::Dynamic, 1>::Zero(2*fe.n_basis());
+        
+        typename SectionPropertyType::value_t
+        mat;
+        
+        typename Eigen::Matrix<scalar_t, Eigen::Dynamic, Eigen::Dynamic>
+        mat1 = Eigen::Matrix<scalar_t, Eigen::Dynamic, Eigen::Dynamic>::Zero(n_strain, 2*fe.n_basis()),
+        mat2 = Eigen::Matrix<scalar_t, Eigen::Dynamic, Eigen::Dynamic>::Zero(2*fe.n_basis(), 2*fe.n_basis());
+
+        MAST::Numerics::FEMOperatorMatrix<scalar_t>
+        Bxmat;
+        Bxmat.reinit(n_strain, 2, fe.n_basis());
+
+        
+        for (uint_t i=0; i<fe.n_q_points(); i++) {
+            
+            c.qp = i;
+            
+            _property->value(c, mat);
+            MAST::Physics::Elasticity::linear_continuum_strain
+            <scalar_t, scalar_t, FEVarType, Dim>(*_fe_var_data, i, epsilon, Bxmat);
+            stress = mat * epsilon;
+            Bxmat.vector_mult_transpose(vec, stress);
+            res += fe.detJxW(i) * vec;
+            
+            if (jac) {
+                
+                Bxmat.left_multiply(mat1, mat);
+                Bxmat.right_multiply_transpose(mat2, mat1);
+                (*jac) += fe.detJxW(i) * mat2;
+            }
+        }
+    }
+
+    template <typename ScalarFieldType>
+    inline void derivative(ContextType& c,
+                           const ScalarFieldType& f,
+                           vector_t& res,
+                           matrix_t* jac = nullptr) const {
+        
+        Assert0(_fe_var_data, "FE data not initialized.");
+        Assert0(_property, "Section property not initialized");
+        
+        const typename FEVarType::fe_basis_t
+        &fe = _fe_var_data->get_fe_shape_data();
+
+        typename Eigen::Matrix<scalar_t, n_strain, 1>
+        epsilon,
+        stress;
+        typename Eigen::Matrix<scalar_t, Eigen::Dynamic, 1>
+        vec     = Eigen::Matrix<scalar_t, Eigen::Dynamic, 1>::Zero(2*fe.n_basis());
+
+        typename SectionPropertyType::value_t
+        mat;
+        typename Eigen::Matrix<scalar_t, Eigen::Dynamic, Eigen::Dynamic>
+        mat1 = Eigen::Matrix<scalar_t, Eigen::Dynamic, Eigen::Dynamic>::Zero(n_strain, 2*fe.n_basis()),
+        mat2 = Eigen::Matrix<scalar_t, Eigen::Dynamic, Eigen::Dynamic>::Zero(2*fe.n_basis(), 2*fe.n_basis());
+
+        MAST::Numerics::FEMOperatorMatrix<scalar_t>
+        Bxmat;
+        Bxmat.reinit(n_strain, 2, fe.n_basis());
+
+        
+        for (uint_t i=0; i<fe.n_q_points(); i++) {
+            
+            c.qp = i;
+            
+            _property->derivative(c, f, mat);
+            MAST::Physics::Elasticity::linear_continuum_strain
+            <scalar_t, scalar_t, FEVarType, Dim>(*_fe_var_data, i, epsilon, Bxmat);
+            stress = mat * epsilon;
+            Bxmat.vector_mult_transpose(vec, stress);
+            res += fe.detJxW(i) * vec;
+            
+            if (jac) {
+                
+                Bxmat.left_multiply(mat1, mat);
+                Bxmat.right_multiply_transpose(mat2, mat1);
+                (*jac) += fe.detJxW(i) * mat2;
+            }
+        }
+    }
+
+    
+protected:
+    
+    
+    const SectionPropertyType       *_property;
+    const FEVarType                 *_fe_var_data;
+};
+
+}  // namespace Elasticity
+}  // namespace Physics
+}  // namespace MAST
+
+#endif // __mast_linear_continuum_strain_energy_h__
