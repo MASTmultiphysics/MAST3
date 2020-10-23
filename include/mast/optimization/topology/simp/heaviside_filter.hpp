ffcb21d3960b6983f11e60c688d49d757087c795
Manav Bhatia <bhatiamanav@gmail.com>
2020-10-22 19:33:25 -0500

    added class for heaviside filter and unit test for sensitivity verification

---
+++ include/mast/optimization/topology/simp/heaviside_filter.hpp
@@ -0,0 +1,89 @@
+/*
+* MAST: Multidisciplinary-design Adaptation and Sensitivity Toolkit
+* Copyright (C) 2013-2020  Manav Bhatia and MAST authors
+*
+* This library is free software; you can redistribute it and/or
+* modify it under the terms of the GNU Lesser General Public
+* License as published by the Free Software Foundation; either
+* version 2.1 of the License, or (at your option) any later version.
+*
+* This library is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+* Lesser General Public License for more details.
+*
+* You should have received a copy of the GNU Lesser General Public
+* License along with this library; if not, write to the Free Software
+* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#ifndef __mast_simp_heaviside_filter_h__
+#define __mast_simp_heaviside_filter_h__
+
+// MAST includes
+#include <mast/base/mast_data_types.h>
+#include <mast/base/exceptions.hpp>
+
+namespace MAST {
+namespace Optimization {
+namespace Topology {
+namespace SIMP {
+
+/*!
+ * This class implements the Heaviside filter defined as
+ * \f[ \tilde{v) =  \frac{\tanh(\beta \eta) + \tanh(\beta(\v-\eta)) }{\tanh (\beta \eta) + \tanh (\beta(1-\eta)) } \f]
+ */
+template <typename ScalarType, typename FieldType>
+class HeavisideFilter {
+    
+public:
+    
+    HeavisideFilter():
+    _beta   (0.),
+    _eta    (0.),
+    _v    (nullptr)
+    { }
+    
+    virtual ~HeavisideFilter() {}
+    
+    inline void set_field(const FieldType& v) { _v = &v;}
+    
+    inline void set_parameters(const real_t beta, const real_t eta) {
+        
+        _beta = beta;
+        _eta  = eta;
+    }
+    
+    template <typename ContextType>
+    inline ScalarType value(const ContextType& c) const {
+        
+        Assert0(_v, "Scalar field not initialized");
+        
+        return ((tanh(_beta*_eta)+tanh(_beta*(_v->value(c)-_eta))) /
+                (tanh(_beta*_eta)+tanh(_beta*(1.-_eta))));
+    }
+
+    template <typename ContextType, typename ScalarFieldType>
+    inline ScalarType derivative(const ContextType&     c,
+                                 const ScalarFieldType& f) const {
+        
+        Assert0(_v, "Scalar field not initialized");
+
+        return ((1.-pow(tanh(_beta*(_v->value(c)-_eta)),2))*
+                _beta*_v->derivative(c, f) /
+                (tanh(_beta*_eta)+tanh(_beta*(1.-_eta))));
+    }
+
+    
+private:
+    
+    real_t                   _beta;
+    real_t                   _eta;
+    const FieldType         *_v;
+};
+} // namespace SIMP
+} // namespace Topology
+} // namespace Optimization
+} // namespace MAST
+
+#endif  // __mast_simp_heaviside_filter_h__
---
+++ tests/optimization/topology/simp/heaviside_filter.cpp
@@ -0,0 +1,131 @@
+/*
+* MAST: Multidisciplinary-design Adaptation and Sensitivity Toolkit
+* Copyright (C) 2013-2020  Manav Bhatia and MAST authors
+*
+* This library is free software; you can redistribute it and/or
+* modify it under the terms of the GNU Lesser General Public
+* License as published by the Free Software Foundation; either
+* version 2.1 of the License, or (at your option) any later version.
+*
+* This library is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+* Lesser General Public License for more details.
+*
+* You should have received a copy of the GNU Lesser General Public
+* License along with this library; if not, write to the Free Software
+* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+// Catch includes
+#include "catch.hpp"
+
+// MAST includes
+#include <mast/optimization/topology/simp/heaviside_filter.hpp>
+
+// Test includes
+#include <test_helpers.h>
+
+namespace MAST {
+namespace Test {
+namespace Optimization {
+namespace Topology {
+namespace SIMP {
+
+struct Context {};
+
+template <typename ScalarType>
+struct DensityField {
+    
+    DensityField(): v(0.5), dv(0.75) {}
+    
+    inline ScalarType
+    value(const Context& c) const { return v;}
+    
+    template <typename ScalarFieldType>
+    inline ScalarType
+    derivative(const Context& c, const ScalarFieldType& f) const
+    {
+        return dv;
+    }
+    
+    ScalarType v;
+    real_t     dv;
+};
+
+inline void test_heaviside_sensitivity()  {
+
+    
+    Context c;
+    DensityField<real_t> field;
+    MAST::Optimization::Topology::SIMP::HeavisideFilter
+    <real_t, DensityField<real_t>>
+    density;
+
+    density.set_field(field);
+    density.set_parameters(10., 0.2);
+    
+    real_t
+    rho     = density.value(c),
+    drho    = density.derivative(c, field),
+    drho_cs = 0.,
+    drho_ad = 0.;
+    
+    // complex step sensitivity
+    {
+        DensityField<complex_t> field_cs;
+        field_cs.v += complex_t(0., ComplexStepDelta);
+        
+        MAST::Optimization::Topology::SIMP::HeavisideFilter
+        <complex_t, DensityField<complex_t>>
+        density_cs;
+
+        density_cs.set_field(field_cs);
+        density_cs.set_parameters(10., 0.2);
+        
+        complex_t
+        rho_cs  = density_cs.value(c);
+
+        drho_cs = field_cs.dv * rho_cs.imag()/ComplexStepDelta;
+
+        CHECK(drho == Catch::Detail::Approx(drho_cs));
+    }
+
+#if MAST_ENABLE_ADOLC == 1
+    // automatic differentiation
+    {
+        DensityField<adouble_tl_t> field_ad;
+        field_ad.v.setADValue(&field_ad.dv);
+        
+        MAST::Optimization::Topology::SIMP::HeavisideFilter
+        <adouble_tl_t, DensityField<adouble_tl_t>>
+        density_ad;
+
+        density_ad.set_field(field_ad);
+        density_ad.set_parameters(10., 0.2);
+        
+        adouble_tl_t
+        rho_ad  = density_ad.value(c);
+
+        drho_ad = *rho_ad.getADValue();
+
+        CHECK(drho == Catch::Detail::Approx(drho_ad));
+    }
+#endif
+}
+
+
+
+TEST_CASE("heaviside_filter_sensitivity",
+          "[Optimization][Topology][SIMP][ComplexStep][AdolC]") {
+    
+    test_heaviside_sensitivity();
+}
+
+} // namespace SIMP
+} // namespace Topology
+} // namespace Optimization
+} // namespace Test
+} // namespace MAST
+
+
