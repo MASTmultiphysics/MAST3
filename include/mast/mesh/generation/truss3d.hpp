5a4230b11cf3d2d222a59f8081e84ee86a3655c7
Manav Bhatia <bhatiamanav@gmail.com>
2020-11-07 16:30:55 -0600

    modifying element iterators from local to active_local element.

--- include/mast/mesh/generation/truss3d.hpp
+++ include/mast/mesh/generation/truss3d.hpp
@@ -467,8 +467,8 @@ struct Truss3D {
         val     = 0.;
         
         libMesh::MeshBase::const_element_iterator
-        e_it  = c.mesh->local_elements_begin(),
-        e_end = c.mesh->local_elements_end();
+        e_it  = c.mesh->active_local_elements_begin(),
+        e_end = c.mesh->active_local_elements_end();
 
         std::set<const libMesh::Node*> nodes;
         
