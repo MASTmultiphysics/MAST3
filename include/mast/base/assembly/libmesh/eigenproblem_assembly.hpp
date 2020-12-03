ea75dccbbfee7489be647a0a984f90665337fae1
Manav Bhatia <bhatiamanav@gmail.com>
2020-12-03 15:48:02 -0600

    Added method for global assembly for eigenproblem sensitivity analysis.

--- include/mast/base/assembly/libmesh/eigenproblem_assembly.hpp
+++ include/mast/base/assembly/libmesh/eigenproblem_assembly.hpp
@@ -114,7 +114,69 @@ public:
             MAST::Numerics::Utility::finalize(B);
         }
     }
+
     
+    template <typename VecType,
+              typename MatType,
+              typename ContextType,
+              typename ScalarFieldType>
+    inline void sensitivity_assemble(ContextType            &c,
+                                     const ScalarFieldType  &f,
+                                     const VecType          &X,
+                                     MatType                &A,
+                                     MatType                &B) {
+                
+        MAST::Numerics::Utility::setZero(A);
+        MAST::Numerics::Utility::setZero(B);
+        
+        // iterate over each element, initialize it and get the relevant
+        // analysis quantities
+        typename MAST::Base::Assembly::libMeshWrapper::Accessor<ScalarType, VecType>
+        sol_accessor(*c.sys, X);
+
+        using elem_vector_t = typename ElemOpsType::vector_t;
+        using elem_matrix_t = typename ElemOpsType::matrix_t;
+        
+        elem_matrix_t
+        A_e,
+        B_e;
+        
+        libMesh::MeshBase::const_element_iterator
+        el     = c.mesh->active_local_elements_begin(),
+        end_el = c.mesh->active_local_elements_end();
+        
+        for ( ; el != end_el; ++el) {
+            
+            // set element in the context, which will be used for the initialization routines
+            c.elem = *el;
+            
+            sol_accessor.init(*c.elem);
+            
+            A_e.setZero(sol_accessor.n_dofs(), sol_accessor.n_dofs());
+            B_e.setZero(sol_accessor.n_dofs(), sol_accessor.n_dofs());
+            
+            // perform the element level calculations
+            _e_ops->derivative(c, f, sol_accessor, A_e, B_e);
+                        
+            // constrain the quantities to account for hanging dofs,
+            // Dirichlet constraints, etc.
+            MAST::Base::Assembly::libMeshWrapper::constrain_and_add_matrix
+            <ScalarType, MatType, elem_matrix_t>
+            (A, c.sys->get_dof_map(), sol_accessor.dof_indices(), A_e);
+            
+            MAST::Base::Assembly::libMeshWrapper::constrain_and_add_matrix
+            <ScalarType, MatType, elem_matrix_t>
+            (B, c.sys->get_dof_map(), sol_accessor.dof_indices(), B_e);
+        }
+
+        // parallel matrix require finalization of communication
+        if (_finalize_jac) {
+            
+            MAST::Numerics::Utility::finalize(A);
+            MAST::Numerics::Utility::finalize(B);
+        }
+    }
+
 private:
 
     bool         _finalize_jac;
