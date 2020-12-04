482c2fd36583da62fc521e62c5b2c30c19f47c82
Manav Bhatia <bhatiamanav@gmail.com>
2020-12-03 21:03:28 -0600

    tests for sensitivity of aggregated minimum.

--- tests/optimization/aggregation/discrete_aggregation.cpp
+++ tests/optimization/aggregation/discrete_aggregation.cpp
@@ -44,20 +44,57 @@ TEST_CASE("discrete_aggregation",
     n  = 10;
     
     Eigen::Matrix<real_t, Eigen::Dynamic, 1>
-    vals = Eigen::Matrix<real_t, Eigen::Dynamic, 1>::Random(n);
+    vals  = Eigen::Matrix<real_t, Eigen::Dynamic, 1>::Random(n),
+    dvals = Eigen::Matrix<real_t, Eigen::Dynamic, 1>::Random(n);
     
     std::vector<real_t>
-    vec (vals.data(), vals.data()+n);
+    vec  (vals.data(), vals.data()+n),
+    dvec (dvals.data(), dvals.data()+n);
 
     real_t
-    min_val = *std::min_element(vec.begin(), vec.end()),
-    min_agg = MAST::Optimization::Aggregation::aggregate_minimum(vec, 100),
-    max_val = *std::max_element(vec.begin(), vec.end()),
-    max_agg = MAST::Optimization::Aggregation::aggregate_maximum(vec, 100);
+    p        = 100,
+    min_val  = *std::min_element(vec.begin(), vec.end()),
+    min_agg  = MAST::Optimization::Aggregation::aggregate_minimum(vec, p),
+    dmin_val = MAST::Optimization::Aggregation::aggregate_minimum_sensitivity(vec, dvec, p),
+    max_val  = *std::max_element(vec.begin(), vec.end()),
+    max_agg  = MAST::Optimization::Aggregation::aggregate_maximum(vec, p);
 
     CHECK(min_val == Catch::Detail::Approx(min_agg).margin(1.e-2));
     CHECK(max_val == Catch::Detail::Approx(max_agg).margin(1.e-2));
 
+    // complex step sensitivity of each component
+    std::vector<complex_t>
+    vec_cs(n);
+    
+    complex_t
+    val;
+
+    real_t
+    dval         = 0.,
+    dval_cs      = 0.,
+    dval_agg_min = 0.,
+    dval_agg_max = 0.;
+    
+    for (uint_t i=0; i<n; i++) {
+        
+        // add perturbation to the ith component
+        for (uint_t j=0; j<n; j++) vec_cs[j] = vec[j];
+        vec_cs[i] += complex_t(0., 1.e-12);
+
+        // sensitivity of minimum aggregate
+        // analytical sensitivity wrt ith var
+        dval = MAST::Optimization::Aggregation::aggregate_minimum_sensitivity(vec, i, 1., p);
+        
+        // complex-step sensitivity wrt ith var
+        val     = MAST::Optimization::Aggregation::aggregate_minimum(vec_cs, p);
+        dval_cs = val.imag()/1.e-12;
+        
+        dval_agg_min += dval_cs * dvec[i];
+        CHECK(dval_cs == Catch::Detail::Approx(dval));
+    }
+
+    // check aggregated sensitivity
+    CHECK(dval_agg_min == Catch::Detail::Approx(dmin_val));
 }
 
 } // namespace Aggregation
