a8c7482b7f93be9711c249f5dc3a9bb1f0154cec
Manav Bhatia <bhatiamanav@gmail.com>
2020-12-06 15:37:25 -0600

    added method to compute discrete maximum sensitivity using cached values of v_max and denom.

--- include/mast/optimization/aggregation/discrete_aggregation.hpp
+++ include/mast/optimization/aggregation/discrete_aggregation.hpp
@@ -125,17 +125,17 @@ aggregate_minimum_sensitivity(const std::vector<ScalarType> &vec,
 /*!
  * Computes the denominator of the sensitivity of aggregated minimum function for use in later sensitivity computations.
  * The value is  \f$  \sum_i \exp (-p (v_i - v_{min}))   \f$,
- * where, \f$ v_{min} \f$ is the minimum value out of all values in \p vec. The demonimator is returned in
+ * where, \f$ v_{min} \f$ is the minimum value out of all values in \p vec. The denomimator is returned in
  * \p denom and the minimum value is returned in \p vmin.
  */
 template <typename ScalarType>
 void
 aggregate_minimum_denominator(const std::vector<ScalarType> &vec,
                               const real_t                   p,
-                              ScalarType                    &demon,
+                              ScalarType                    &denom,
                               ScalarType                    &v_min) {
     
-    demon  = 0.;
+    denom  = 0.;
     
     v_min = MAST::Numerics::Utility::real_minimum(vec);
 
@@ -183,7 +183,7 @@ template <typename ScalarType>
 ScalarType
 aggregate_minimum_sensitivity(const std::vector<ScalarType> &vec,
                               const std::vector<ScalarType> &dvec,
-                              const real_t                   p
+                              const real_t                   p,
                               const ScalarType              &denom,
                               const ScalarType              &v_min) {
     
@@ -195,9 +195,7 @@ aggregate_minimum_sensitivity(const std::vector<ScalarType> &vec,
         dv += exp(-p * (vec[i] - v_min)) * dvec[i];
     }
     
-    v = dv / denom;
-    
-    return v;
+    return dv / denom;
 }
 
 
@@ -291,6 +289,84 @@ aggregate_maximum_sensitivity(const std::vector<ScalarType> &vec,
     
     return v;
 }
+
+
+/*!
+ * Computes the denominator of the sensitivity of aggregated maximum function for use in later sensitivity computations.
+ * The value is  \f$  \sum_i \exp (p (v_i - v_{max}))   \f$,
+ * where, \f$ v_{max} \f$ is the maximum value out of all values in \p vec. The denominator is returned in
+ * \p denom and the minimum value is returned in \p v_max.
+ */
+template <typename ScalarType>
+void
+aggregate_maximum_denominator(const std::vector<ScalarType> &vec,
+                              const real_t                   p,
+                              ScalarType                    &denom,
+                              ScalarType                    &v_max) {
+    
+    denom  = 0.;
+    
+    v_max = MAST::Numerics::Utility::real_maximum(vec);
+
+    for (uint_t i=0; i<vec.size(); i++) {
+        
+        denom  += exp(p * (vec[i] - v_max));
+    }
+}
+
+
+
+/*!
+ * computes sensitivity of aggregated maximum of values specified in vector \p vec with respect to \p i th value.
+ * The aggregation constant is \p p.
+ * The aggregation expression used is
+ * \f[ \frac{d v_{agg}}{d v_j} =  \frac{ \exp (p (v_j - v_{max})) }{  \sum_i \exp (p (v_i - v_{max})) }  \f],
+ * where, \f$ v_{max} \f$ is the maximum value out of all values in \p vec. This method differs from the other
+ * in that the user provides the cached denominator of the sensitivity  and the maximum value in \p denom and
+ * \p v_max.
+ */
+template <typename ScalarType>
+ScalarType
+aggregate_maximum_sensitivity(const std::vector<ScalarType> &vec,
+                              const uint_t                   i,
+                              const real_t                   p,
+                              const ScalarType              &denom,
+                              const ScalarType              &v_max) {
+    
+    return exp(p * (vec[i] - v_max)) / denom;
+}
+
+
+/*!
+ * computes sensitivity of aggregated maximum of values specified in vector \p vec with respect to
+ * parameter \f$ \alpha \f$.  The aggregation constant is \p p. The sensitivity of values with respect to parameter is
+ * provided in \p dvec.
+ * The sensitivity expression used is
+ * \f[ \frac{d v_{agg}}{d p} =  \frac{ \sum_j \exp (p (v_j - v_{max})) \frac{dv_j}{d\alpha} }{  \sum_i \exp (p (v_i - v_{max})) }  \f],
+ * where, \f$ v_{max} \f$ is the maximum value out of all values in \p vec.
+ * This method differs from the other
+ * in that the user provides the cached denominator of the sensitivity  and the maximum value in \p denom and
+ * \p v_max.
+ */
+template <typename ScalarType>
+ScalarType
+aggregate_maximum_sensitivity(const std::vector<ScalarType> &vec,
+                              const std::vector<ScalarType> &dvec,
+                              const real_t                   p,
+                              const ScalarType              &denom,
+                              const ScalarType              &v_max) {
+    
+    ScalarType
+    dv     = 0.;
+    
+    for (uint_t i=0; i<vec.size(); i++) {
+        
+        dv += exp(p * (vec[i] - v_max)) * dvec[i];
+    }
+    
+    return dv / denom;
+}
+
 
 } // Aggregation
 } // Optimization
